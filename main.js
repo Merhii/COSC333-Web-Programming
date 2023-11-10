bbce21e0efbccc82267a6094606d158607c6ea2c
bobdayraki <i.dayraki@gmail.com>
2023-11-10 18:25:01 +0300

    updated home page

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
 $(document).ready(function(){
+    // reserve car
     $("#reserveBtn").click(function(){
         alert(" Your Car In'sha'alah will be reserved");
     })
 })
-console.log("Hello World")
+
+  document.getElementById("menu-trigger").addEventListener('click', function(){
+    document.getElementById("navbarList").classList.toggle('nav-menu-open')
+  })
+
+
+  $(function () {
+    $(document).scroll(function () {
+        var $nav = $(".test-navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
