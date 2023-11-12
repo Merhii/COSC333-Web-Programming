8dd8e20aac09670b340e55e8f0f13d546a8dccee
bobdayraki <i.dayraki@gmail.com>
2023-11-12 16:15:11 +0300

    garage update

---
+++ garage.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  
+  // Hamburger menu trigger
+var menuTrigger = $("#menu-trigger");
+menuTrigger.click(function() {
+$("#navbarList").toggleClass("nav-menu-open");
+});
+
+
+});
+
+
