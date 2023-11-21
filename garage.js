c7d4cfe9f9bc6f91407ee5f7e72cb7a996ae29bc
bobdayraki <i.dayraki@gmail.com>
2023-11-21 23:41:19 +0300

    Garage filter cars responsive and fix

--- garage.js
+++ garage.js
@@ -5,24 +5,37 @@ var menuTrigger = $("#menu-trigger");
 menuTrigger.click(function() {
 $("#navbarList").toggleClass("nav-menu-open");
 });
-
+// filter cars with jquery
+// $("#searchbtn").on("click", filtercars);
+// function filtercars() {
+//   var filterValue1 = $("#filter1").val().toLowerCase();
+//   var filterValue2 = $("#filter2").val().toLowerCase();
+  
+//   $(".col-xs-12").each(function() {
+//     var divClass = $(this).attr("class").toLowerCase();
+//     if(divClass.includes(filterValue1) && divClass.includes(filterValue2)) {
+//       $(this).show();
+//     } else {
+//       $(this).hide();
+//     }
+//   });
+// }
 
 });
+// filter cars 
 document.getElementById("searchbtn").addEventListener("click", filtercars)
-document.getElementById("filter1")
-document.getElementById("filter2")
 function filtercars() {
   // Get the input value and convert it to lowercase
-  var filterValue = document.getElementById("filter1").value.toLowerCase();
+  var filterValue1 = document.getElementById("filter1").value.toLowerCase();
   var filterValue2 = document.getElementById("filter2").value.toLowerCase();
   
   // Get all the div elements
-  var divs = document.getElementsByClassName("image-container");
+  var divs = document.getElementsByClassName("col-xs-12");
   
   // Loop through the divs and hide/show them based on their classes
   for(var i = 0; i < divs.length; i++) {
     var divClass = divs[i].className.toLowerCase();
-    if(divClass.includes(filterValue) && divClass.includes(filterValue2)) {
+    if(divClass.includes(filterValue1) && divClass.includes(filterValue2)) {
       divs[i].style.display = "";
     } else {
       divs[i].style.display = "none";
